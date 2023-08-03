51aa37041802c9564a8d7ee3fca8e284a0f62695
Rui Gil <rui@gil.com>
2023-08-03 21:34:13 +0200

    analytics

--- site/_includes/base.layout.ts
+++ site/_includes/base.layout.ts
@@ -13,6 +13,15 @@ export default ( data : PageData) => {
       <script type="module" src="/assets/js/components.js" defer></script>
       <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.5.2/cdn/shoelace-autoloader.js" defer></script>
       ${data.scripts ? data.scripts.map((src: string) => (`<script type="module" src=${src} defer></script>`)) : ""}
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-H0PZ82W0E9"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-H0PZ82W0E9');
+      </script>
     </head>
     <body class="flex flex-col sl-theme-dark">
       ${ toolbar(data) }
