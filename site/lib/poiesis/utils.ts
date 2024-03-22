b383bcf66c24a10b15eec3f67c64d696a1d5928e
Rui Gil <rui@gil.com>
2024-03-22 17:38:20 +0100

    gaussian kernels

--- site/lib/poiesis/utils.ts
+++ site/lib/poiesis/utils.ts
@@ -156,6 +156,32 @@ export const cornell = () => {
     return { vertices, indices, meshes, materials }
 }
 
+const gaussian = (x: number, y: number, sigma: number): number => {
+    const pi = Math.PI;
+    return Math.exp(-(x*x + y*y) / (2.0 * sigma * sigma)) / (2.0 * pi * sigma * sigma);
+}
+
+export const createGaussianKernel = (sigma: number): number[] => {
+    let kernel: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let sum = 0;
+
+    for(let i = 0; i < 9; i++) {
+        let x = i % 3;
+        let y = Math.floor(i / 3);
+        kernel[y * 3 + x] = gaussian(x - 1, y - 1, sigma);
+        sum += kernel[y * 3 + x];
+    }
+
+    // Normalize the kernel
+    for(let i = 0; i < 9; i++) {
+        let x = i % 3;
+        let y = Math.floor(i / 3);
+        kernel[ y * 3 + x ] /= sum;
+    }
+
+    return kernel;
+}
+
 
 export const loadWGSL = async (url: string) => {
     return await (await fetch(url)).text()
