af1e174dafbd644f22795a3e4ce9fb2666115fc3
Rui Gil <rui.gil.net@gmail.com>
2024-07-19 16:02:48 +0200

    resolution change

--- site/shaders/draft/video360/video360.ts
+++ site/shaders/draft/video360/video360.ts
@@ -2,11 +2,6 @@ import { PSpec, Definitions } from "../../../lib/poiesis/index.ts";
 
 export const video360 = async (code:string, defs:Definitions) => {
 
-    //const image360 = new Image();
-    //image360.src = "/assets/img/egypt360.jpg";
-    //await image360.decode();
-
-    //const img360bitmap = await createImageBitmap(image360);
     // Create a video element to capture the video stream
     const video = document.createElement('video');
     video.src = "/assets/video/redsea.mp4";
