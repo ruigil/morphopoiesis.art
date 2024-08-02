fca247142c52a84be868f07f517a960e3585afd2
Rui Gil <rui.gil.net@gmail.com>
2024-08-02 12:46:27 +0200

    parameter update for 60hz

--- site/shaders/wave/wave.ts
+++ site/shaders/wave/wave.ts
@@ -20,12 +20,12 @@ export const wave = (code: string,defs: Definitions ) => {
             uniforms: () => ({
                 uni: {
                     size: [size.x, size.y],
-                    frequency: 29,
+                    frequency: 11,
                     amplitude: 20,
                     osc: [ 
-                        { amplitude: 3, frequency: 11, position: [size.x/2 ,size.y/2 + 100] },
-                        { amplitude: 1, frequency: 7, position: [size.x/2 - 100,size.y/2 - 70] },
-                        { amplitude: 2, frequency: 5, position: [size.x/2 + 100,size.y/2 - 70] }
+                        { amplitude: 3, frequency: 7, position: [size.x/2 ,size.y/2 + 100] },
+                        { amplitude: 1, frequency: 5, position: [size.x/2 - 100,size.y/2 - 70] },
+                        { amplitude: 2, frequency: 2, position: [size.x/2 + 100,size.y/2 - 70] }
                     ]
                 }
             }),
