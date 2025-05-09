af6bba2821c2094ef67877b4d379147676749a00
Rui Gil <rui.gil.net@gmail.com>
2025-05-09 12:07:06 +0200

    delete shader drafts

--- site/shaders/mandelbrot/mandelbrot.ts
+++
@@ -1,9 +0,0 @@
-import { PSpec, Definitions } from "../../lib/poiesis/index.ts";
-
-export const mandelbrot = async (code: string, defs: Definitions, fx: any) => {
-    return (): PSpec => ({ 
-        code: code, 
-        defs: defs,
-        clearColor: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 } // Black background
-    });
-}
