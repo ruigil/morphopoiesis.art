b9869322236dd9ea17207d13172448c0c601b0d3
Rui Gil <rui.gil.net@gmail.com>
2025-05-09 12:08:13 +0200

    typo

--- site/shaders/inversion/inversion.ts
+++ site/shaders/inversion/inversion.ts
@@ -2,8 +2,6 @@ import { PSpec, Definitions } from "../../lib/poiesis/index.ts";
 
 
 export const inversion = (code:string,defs:Definitions, $fx?:any) => {
- 
-    console.log("fx-hash",$fx.hash)
- 
+  
     return (): PSpec => ({ code: code, defs: defs });
 }
